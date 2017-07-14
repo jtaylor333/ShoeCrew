0862f72dc835024623db4a66256273d8590a0497
Hubot <staples8758@gmail.com>
2017-07-14 14:54:22 -0400

    change

--- handlers/main_handler.py
+++ handlers/main_handler.py
@@ -3,12 +3,24 @@ import jinja_env
 import logging
 import webapp2
 
+from google.appengine.api import users
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         logging.info("MainHandler")
+        logging.info(users.get_current_user())
+        logging.info(users.create_login_url('/'))
+
         html_params = {
             "title": "Shoe Crew",
             "content": "It works",
+            "html_login_url": users.create_login_url('/'),
         }
+
+        user = users.get_current_user()
+
+        if user != None:
+            html_params["user_email"] = user.email()
+
         template = jinja_env.env.get_template('templates/tmpl2.html')
         self.response.out.write(template.render(html_params))
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from handlers import jinja_env
 from handlers import main_handler
 from handlers import book_handler
 
+from google.appengine.api import users
+
 jinja_env.init(os.path.dirname(__file__))
 
 app = webapp2.WSGIApplication([

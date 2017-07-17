35c4492359be8616e46fb4ea041f64676862543a
jtaylor333 <jtaylor.jr33@gmail.com>
2017-07-17 15:04:39 -0400

    Day2End

--- handlers/book_handler.py
+++ handlers/book_handler.py
@@ -25,17 +25,20 @@ class BookHandler(webapp2.RequestHandler):
         template = jinja_env.env.get_template('templates/tmpl.html')
         self.response.out.write(template.render(html_params))
 
-        # def post(self):
+    def post(self):
             
-        #     r_name = self.request.get("name")
-        #     r_description = self.request.get("description")
-        #     # logging.info(r_name)
+        r_name = self.request.get("name")
+        r_description = self.request.get("description")
+            # logging.info(r_name)
 
-        #    new_shoe = shoe_upload.ShoeModel(
+        new_shoe = shoe_upload.ShoeU(
 
-        #       shoe_name = r_name,
-        #       shoe_description = r_description,
-        #       self.redirect("/all")
+            shoe_name = r_name,
+            shoe_description = r_description,
+            user_email = "Fix Later",
+              
 
 
-        #     )
+            )
+           new_shoe.put()
+           self.redirect("/all")

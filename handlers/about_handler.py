d77243d3da66bba2ebaeaee0eaf0ca556dd021ff
nigelpowell <nigelpowell3@gmail.com>
2017-07-18 14:01:00 -0400

    css

--- handlers/about_handler.py
+++ handlers/about_handler.py
@@ -5,12 +5,18 @@ import webapp2
 from google.appengine.api import users
 
 
+
+
 class AboutHandler(webapp2.RequestHandler):
+
+	<link rel="stylesheet" type="text/css" href="resources/style.css">
+	<link rel="stylesheet" type="text/css" href="resources/title.css">
+	
     def get(self):
     	logging.info("AboutHandler")
         html_params = {
             "title": "About Page",
-            "content": "Hello, "
+            "content": "Hello, we are the Shoe Crew."
         }
         template = jinja_env.env.get_template('templates/tmpl3.html')
         self.response.out.write(template.render(html_params))
